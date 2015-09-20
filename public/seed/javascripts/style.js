34e6b9b27eb342b7ad843f731b745b566b857025
Rosaniline <xup6qup3@gmail.com>
2015-09-20 23:45:02 +0800

    gsheet update

--- public/seed/javascripts/style.js
+++ public/seed/javascripts/style.js
@@ -64,39 +64,7 @@ $(document).ready(function(){
 
 
 
-	if ( beforeApply() || afterApply() ) {
-		$('#div_apply').css('display', 'none');
-	}
 
-
-	$(document).on("click", "#img_apply_btn, #btn_apply", function(e){
-
-		// Prevent a page reload when a link is pressed
-		e.preventDefault(); 
-
-		if ( beforeApply() ) {	
-
-			bootbox.alert("第二屆臺大種子創業競賽將於 9/21(一) - 9/30(三) 開放系統報名！");
-		}
-
-		else if (afterApply()) {
-
-			bootbox.alert("第二屆臺大種子創業競賽已截止報名。歡迎於 10/30(五) 前來決選觀賽！");
-			
-		}
-
-		else {
-
-			$("html, body").animate({
-
-				scrollTop: $("#div_apply").offset().top - 50
-
-			}, 500);
-		}
-
-		
-
-	});
 
 	$(document).on("click", "#footer1 #link_menu a", function(e){
 
@@ -176,25 +144,7 @@ $(document).ready(function(){
         
     });
 
-    $(document).on('click', '#ss_submit', function(event) {
-    	event.preventDefault();
 
-		$('#ss-form').ajaxSubmit({
-
-            error: function(xhr) {
-				status('Error: ' + xhr.status);
-				console.log('error');
-            },
-
-            success: function(response) {
-				$('#apply_ing').css('display', 'none');
-				$('#apply_done').css('display', 'block');
-            }
-        });
-
-        return false;
-	    	
-    });
 
 	
 
@@ -233,23 +183,7 @@ function alignNavbar(scroll) {
 
 }
 
-function beforeApply() {
 
-	var s = new Date(2015, 8, 20, 0, 0, 0, 0); 
-	var d = new Date();
-	
-	return s > d;
-
-}
-
-function afterApply() {
-
-	var t = new Date(2015, 9, 1, 0, 0, 0, 0); 
-	var d = new Date(); 
-	
-	return d > t;
-
-}
 
 
 
--- routes/upload.js
+++ routes/upload.js
@@ -2,6 +2,7 @@ var multer = require('multer');
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
+var GoogleSpreadsheet = require("google-spreadsheet");
 var router = express.Router();
 
 
@@ -12,15 +13,29 @@ var storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
 
-    // for (var key in req.body){
-    // //   if (req.body.hasOwnProperty(key)) {
-    //     console.log(key + ", " + req.body[key]);
-    // //   }
-    // }
+    var form = req.body;
+    var timestamp = new Date();
 
-    // console.log(req.body);
+    var gsheet = new GoogleSpreadsheet('1gcs4oF9bbF1W1DGJ9RfJjdKqbnsbv0Fnko7zsSYTKfc');
+    var creds = require('./tecapply-e4ea57d63d5e.json');
 
+    gsheet.useServiceAccountAuth(creds, function(err){
 
+      gsheet.getInfo( function( err, sheet_info ){
+
+        gsheet.addRow(sheet_info.worksheets[0].id, {
+          "時間戳記": timestamp.toString(),
+          "團隊名稱": form['team'],
+          "聯絡人": form['contact'],
+          "手機": form['phone'],
+          "電子信箱": form['mail'],
+          "概念名稱": form['idea'],
+          "產業": form['industry']
+        }, function(){
+          console.log("updated.");
+        });
+      });
+    });
 
     var filename = req.body['team'];// path.parse(file.originalname).name;
     var ext = path.extname(file.originalname);
@@ -49,3 +64,4 @@ router.post('/', upload.single('attach_file'), function (req, res, next) {
 });
 
 module.exports = router;
+
